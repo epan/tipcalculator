0e552e1cf17b71384e9c50ad0b48eca97de6b689
Eric Pan <ericpan@gmail.com>
2014-05-29 00:39:43 -0700

    initial commit with working calculator

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Eric Pan on 5/28/14.
+//  Copyright (c) 2014 Eric Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
