0626373f04fa35f4738b4dd28274c502ab63ca61
Eric Pan <ericpan@gmail.com>
2014-05-28 23:43:35 -0700

    Initial Commit

---
+++ tipcalculator/EXEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EXEAppDelegate.h
+//  tipcalculator
+//
+//  Created by Eric Pan on 5/28/14.
+//  Copyright (c) 2014 Eric Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EXEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Eric Pan on 5/28/14.
+//  Copyright (c) 2014 Eric Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EXEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EXEAppDelegate class]));
+    }
+}
